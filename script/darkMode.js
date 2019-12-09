92260cc901e7b51c56c9b1fb795fa749a99e3552
edgsel <edgar.selihov@gmail.com>
2019-12-09 23:49:02 +0200

    navbar now is visible when scrolled (small trouble with laggy scroll)

--- script/darkMode.js
+++ script/darkMode.js
@@ -5,16 +5,16 @@ function nightModeOff() {
     $(".active").css({"border-bottom": "1px solid black"});
     $(".tabs").css({"color": "#222"});
     $(".letters").css({"color": "#222"});
+    $(".png").css({"filter": "none"});
+    $("article").removeClass("white-shadow");
+    $("article").addClass("shadow");
+    $(".profile").css({"border" : "0.5px solid black"});
+    $(".blurred").css({"background": "#f3f0f0"});
 
     $(".tabs").hover(function(e) {
         $(this).css("background-color",e.type === "mouseenter"?"black":"transparent");
         $(this).css("color",e.type === "mouseenter"?"white":"#222");
     });
-
-    $(".png").css({"filter": "none"});
-    $("article").removeClass("white-shadow");
-    $("article").addClass("shadow");
-    $(".profile").css({"border" : "0.5px solid black"});
 }
 
 function nightModeOn() {
@@ -22,18 +22,32 @@ function nightModeOn() {
     $(".active").css({"border-bottom": "1px solid #e6e6e6"});
     $(".tabs").css({"color": "#e6e6e6"});
     $(".letters").css({"color": "#e6e6e6"});
-    $(".tabs").hover(function (e) {
-        $(this).css("background-color",e.type === "mouseenter"?"#e6e6e6":"transparent");
-        $(this).css("color",e.type === "mouseenter"?"#222":"#e6e6e6");
-    });
-
     $(".png").css({"filter": "grayscale(1) invert(.9)"});
     $("article").removeClass("shadow");
     $("article").addClass("white-shadow");
     $(".profile").css({"border" : "0.5px solid #e6e6e6"});
+    $(".blurred").css({"background": "#1d1d1d"});
+
+    $(".tabs").hover(function (e) {
+        $(this).css("background-color",e.type === "mouseenter"?"#e6e6e6":"transparent");
+        $(this).css("color",e.type === "mouseenter"?"#222":"#e6e6e6");
+    });
 }
 
 $(function () { //$( document ).ready(function() equivalent
+
+    $(document).scroll(function () {
+
+        let $nav = $(".div");
+
+        if ( $("body").hasClass("dark-mode")) {
+            $(".blurred").css({"background": "#1d1d1d"});
+        } else {
+            $(".blurred").css({"background": "#f3f0f0"});
+        }
+        $(".div").stop();
+        $nav.toggleClass('blurred', $(this).scrollTop() > $nav.height());
+    });
     /* load theme according to local storage value */
     if (localStorage.getItem('isDarkMode') == 1) {
         nightModeOn();
@@ -57,7 +71,6 @@ $(function () { //$( document ).ready(function() equivalent
 
         } else if (!$("body").hasClass("dark-mode")) {
             nightModeOn();
-            /* Change local storage value for saving theme */
             localStorage.setItem("isDarkMode", 1);
         }
     });
